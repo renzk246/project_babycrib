6b9adc6bc037eeed33c9dfa663682772bef23b33
Renzo Monteadora <renzk246@gmail.com>
2023-04-03 19:42:44 +0800

    4.2

--- setup.sh
+++ setup.sh
@@ -2,25 +2,25 @@
 
 # Update Raspi to Latest Version
 sudo apt update
-sudo apt full-upgrade -y
+sudo apt full-upgrade --yes
 
 # Install Gpiozero
 sudo apt update
 sudo pip3 install gpiozero
 
 # Python3 Dev
-sudo apt-get install python3-dev -y
+sudo apt-get install python3-dev --yes
 
 # Tensorflow Lite Intallation
 sudo apt update
-sudo apt upgrade -y
+sudo apt upgrade --yes
 echo "deb [signed-by=/usr/share/keyrings/coral-edgetpu-archive-keyring.gpg] https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/coral-edgetpu-archive-keyring.gpg >/dev/null
 sudo apt update
 sudo apt install python3-tflite-runtime libatlas-base-dev -y
 
 # Download Tensorflow Lite Model
-sudo apt install git -y
+sudo apt install git --yes
 git clone https://github.com/tensorflow/examples --depth 1
 mv ./examples ./tensorflow_model
 cd ./tensorflow_model/lite/examples/audio_classification/raspberry_pi
@@ -29,7 +29,7 @@ sudo ./setup.sh
 cd ~
 
 # Pyaudio
-#sudo apt install portaudio19-dev python3-pyaudio -y
+# sudo apt install portaudio19-dev python3-pyaudio -y
 
 # Librosa (Optional)
 # pip3 install librosa
