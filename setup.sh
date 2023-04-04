bbd3e241dd8a9a147c8e7f801f2903996826c738
Renzo Monteadora <renzk246@gmail.com>
2023-04-04 16:31:37 +0800

    4.3

--- setup.sh
+++ setup.sh
@@ -1,33 +1,30 @@
 # Update Raspi to Latest Version
 sudo apt update
-sudo apt full-upgrade --yes
+sudo apt full-upgrade
 
 # Install Gpiozero
 sudo apt update
 sudo pip3 install gpiozero
 
 # Python3 Dev
-sudo apt-get install python3-dev --yes
+sudo apt-get install python3-dev
 
 # Tensorflow Lite Installation
 sudo apt update
-sudo apt upgrade --yes
+sudo apt upgrade
 echo "deb [signed-by=/usr/share/keyrings/coral-edgetpu-archive-keyring.gpg] https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/coral-edgetpu-archive-keyring.gpg >/dev/null
 sudo apt update
-sudo apt install python3-tflite-runtime libatlas-base-dev --yes
+sudo apt install python3-tflite-runtime libatlas-base-dev
 
 # Download Tensorflow Lite Model
-#sudo apt install git --yes
-#git clone https://github.com/tensorflow/examples --depth 1
-#mv ./examples ./tensorflow_model
-#cd ./tensorflow_model/lite/examples/audio_classification/raspberry_pi
-#chmod +x ./setup.sh
-#sudo ./setup.sh
-#cd ~
-
-# Install Python dependencies
-python3 -m pip install -r requirements.txt
+sudo apt install git --yes
+git clone https://github.com/tensorflow/examples --depth 1
+mv ./examples ./tensorflow_model
+cd ./tensorflow_model/lite/examples/audio_classification/raspberry_pi
+chmod +x ./setup.sh
+sudo ./setup.sh
+cd ~
 
 # Install Adafruit DHT11 Library
 pip3 install adafruit-circuitpython-dht
